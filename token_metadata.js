ba7ce537f25c1be10c8d851f800bc8a199f986df
Pala1998 <165445247+Pala1998@users.noreply.github.com>
2024-03-30 03:26:57 +0100

    Add files via upload

---
+++ token_metadata.js
@@ -0,0 +1,6 @@
+{
+  "name": "FuFaGuRu",
+  "symbol": "FG",
+  "description": "Fufa guru stai a casa",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple122/v4/03/37/23/03372373-920f-80c0-6728-a51f82e1d2a2/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp"
+}
